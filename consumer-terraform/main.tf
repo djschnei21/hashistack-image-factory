2a33c0ed122c6a1553bb63ae629ee74a16f9c4bb
djschnei21 <32646250+djschnei21@users.noreply.github.com>
2024-05-08 18:21:08 -0400

    consumer first commit

---
+++ consumer-terraform/main.tf
@@ -0,0 +1,24 @@
+terraform {
+  required_providers {
+    hcp = {
+      source = "hashicorp/hcp"
+      version = "0.88.0"
+    }
+
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.8.0"
+    }
+  }
+}
+
+provider "hcp" {}
+
+provider "aws" {}
+
+data "hcp_packer_artifact" "tomcat-ubuntu" {
+  bucket_name   = "tomcat-ubuntu"
+  channel_name  = "latest"
+  platform      = "aws"
+  region        = "us-east-2"
+}
