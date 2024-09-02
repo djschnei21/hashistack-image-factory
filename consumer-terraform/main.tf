eda12e924f0c709b1e6ae5b91366dd240900fa01
djschnei21 <32646250+djschnei21@users.noreply.github.com>
2024-09-02 18:50:04 -0400

    swap

--- consumer-terraform/main.tf
+++ consumer-terraform/main.tf
@@ -16,15 +16,15 @@ provider "hcp" {}
 
 provider "aws" {}
 
-data "hcp_packer_artifact" "tomcat-ubuntu" {
-  bucket_name   = "tomcat-ubuntu"
+data "hcp_packer_artifact" "apache-ubuntu" {
+  bucket_name   = "apache-ubuntu"
   channel_name  = "release"
   platform      = "aws"
   region        = "us-east-2"
 }
 
-resource "aws_instance" "tomcat" {
-  ami           = data.hcp_packer_artifact.tomcat-ubuntu.external_identifier
+resource "aws_instance" "apache" {
+  ami           = data.hcp_packer_artifact.apache-ubuntu.external_identifier
   instance_type = "t3.micro"
 
   tags = {
